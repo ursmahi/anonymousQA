817d95c3b0f6f4280f5a5564d63bf1b293ed28b6
Mahidhar Kakumani <43433128+ursmahi@users.noreply.github.com>
2022-11-25 17:34:48 +0530

    prod check

--- pages/api/userQA.js
+++ pages/api/userQA.js
@@ -40,6 +40,7 @@ export default async function handler(req, res) {
             }
             });
         } else {
+            console.log("NOT FOUND")
             res.status(200).json([]);
         }
     }
